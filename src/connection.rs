0569bdd4aa1a8742e3a5033775cf001b08ce197a
Graeme Read <graeme@sigma957.net>
2025-01-04 14:53:48 +0000

    Respond to multiple PINGs (WY1)

--- src/connection.rs
+++ src/connection.rs
@@ -19,40 +19,48 @@ impl Connection {
     }
 
     pub fn process(&mut self) -> Result<()> {
-        loop {
-            let mut buffer = [0; 1024];
-            let read = self.stream.read(&mut buffer)?;
-            println!("read {read} bytes from stream");
+        'command: loop {
+            loop {
+                let mut buffer = [0; 1024];
+                let read = self.stream.read(&mut buffer)?;
+                println!("read {read} bytes from stream");
 
-            if read == 0 {
-                println!("nothing to read from stream, exiting process loop");
-                break;
+                if read == 0 {
+                    if buffer.is_empty() {
+                        println!("no commands left from client, shutting down connection");
+                        break 'command;
+                    }
+
+                    println!("nothing to read from stream, exiting process loop");
+                    break;
+                }
+
+                self.buffer.extend_from_slice(&buffer[..read]);
+
+                if read < buffer.len() {
+                    println!("read less than buffer size in, exiting process loop");
+                    break;
+                }
             }
 
-            self.buffer.extend_from_slice(&buffer[..read]);
-
-            if read < buffer.len() {
-                println!("read less than buffer size in, exiting process loop");
-                break;
+            if let Ok(request) = String::from_utf8(self.buffer.clone()) {
+                dbg!(request);
             }
-        }
 
-        if let Ok(request) = String::from_utf8(self.buffer.clone()) {
-            dbg!(request);
-        }
-
-        let request = RedisType::try_from(self.buffer.as_slice())?;
-        let response = match request {
-            RedisType::Array(array) if array.contains(&RedisType::BulkString(b"PING")) => {
-                RedisType::SimpleString("PONG")
+            let request = RedisType::try_from(self.buffer.as_slice())?;
+            let response = match request {
+                RedisType::Array(array) if array.contains(&RedisType::BulkString(b"PING")) => {
+                    RedisType::SimpleString("PONG")
+                }
+                _ => RedisType::SimpleError("ERR unknown command"),
             }
-            _ => RedisType::SimpleError("ERR unknown command"),
+            .encode();
+            if let Ok(response) = String::from_utf8(response.clone()) {
+                dbg!(response);
+            }
+            self.stream.write_all(response.as_slice())?;
+            self.buffer.drain(0..);
         }
-        .encode();
-        if let Ok(response) = String::from_utf8(response.clone()) {
-            dbg!(response);
-        }
-        self.stream.write_all(response.as_slice())?;
         self.stream.shutdown(Shutdown::Both)?;
 
         Ok(())
